09760ac88ba7581dd8aa8c2b7fbf736ecf65f0a0
abdulbadii <abdulbadii@yandex.com>
2024-03-06 20:25:42 +0700

    Update selector.js

--- selector.js
+++ selector.js
@@ -1,72 +1,63 @@
 #!/usr/bin/env node
-
 const {stdin, stdout, exit} = process;
 let j, elems=[];
-isElmBlncd = s =>{
- let i, sig = s.match(/^\s*<!DOCTYPE\s\w+>/) ||exit(1);
- i= sig.index + sig[0].length;
+
+isHTMvalid = s =>{
+ let i, sig = s.match(/^\s*<!DOCTYPE\s\w+>[^<>]*/) ||exit(1);
+ i= sig[0].length;
  let stack=[], ind='';
  for (;i < s.length;) {
-  let c = s[i], nextI, elmn;
-  if (c == '<') {
-   nextI = s.indexOf( '>', i);
+  const stRE = s.slice(i),
+  h = stRE.match(/^<\/?([a-z][-\w]*)[^<>]*>/);
+  if( h) {
    const
-   head = s.substring( i++, ++nextI),
-   t = head.match(/^<(?:([a-z][-\w]*)[^<>]*|(\/[a-z][-\w]*)|!--.*--)>$/) ||exit(1),
+   head = h[0],
+   tag  = h[1];
+   /*Large atrribute is put as the first/last a number of char, so is such large*/
    eHead =
-   head.length > 410? head.substring(0,150)+'.....'+head.substring( head.length-130): head;
-   let
-   str, tag, indEl = ind + eHead;
-   if (/^<(?:meta|link|input|img|hr|base)\b|^<!/.test(eHead)) {
-    elems.push( indEl);
-    i = nextI
-   } else if (tag=t[1]) {
-    if( /script|style|title|path/.test(tag) ) {
-     i = s.indexOf('</'+tag+'>',nextI)
-     if (i < 0) return [false, stack+','+tag];
-     i += 3 +tag.length;
-     //large text node is put as the first and last a number of char
-     if (i-nextI > 799)
-      elmn = indEl + s.substring( nextI, nextI +210) +'.....'+ s.substring( i-190, i);
-     else
-      elmn = indEl + s.substring( nextI, i)
-    }
-    else if( str =s.substring( nextI).match(/^([^<>]*)<\/([a-z][-\w]*)>/)) {
-     if (tag != str[2]) return [false, stack+','+tag];
-     i = nextI + str[0].length;
-     elmn = indEl + (str[1]? '\n  '+ind+str[1] :'') +'\n  '+ind+'</'+str[2]+'>'
-    }
-    else if( str = s.substring( nextI).match(
-    /^([^><]+)(<([a-z][-\w]*)(?:\s+[^>]+)?>)?/)) {
-     if (!str[2]) return [false, stack+','+tag];
-     stack.push( tag);
-     stack.push( str[3]);
-     elmn = indEl + str[1];
-     i = nextI + str[0].length;
-     ind += ' '
-    }
-    else {
-     stack.push( tag);
-     elmn = indEl;
-     i = nextI;
-     ind += ' ';
-    }
-    elems.push( elmn);
-    !j && tag=='div'? j= elems.length+3: null;
+   head.length <410? head: head.slice(0,170)+'.....'+head.slice(-133);
+   if (head[1] != '/') {
+    let
+    indtE = ind + eHead,
+    cRE; 
+    if( !tag.search(/^(meta|link|input|img|hr|base)\b/)) {
+     elems.push( indtE);
+     i += head.length
+    } else if( cRE=/^<(script|style|title|path)\b[^<>]*>(.*?)(<\/\1>|$)/s.exec(stRE) ) {
+     if (cRE[3]) {
+      let l;
+      elems.push( indtE
+      + ( (l=cRE[2].length) <730? cRE[2]: cRE[2].slice(0,210) +'.....'+ cRE[2].slice(-170))
+      + cRE[3] );
+      i += head.length + l + cRE[3].length
+     }
+     else return [false, stack+','+tag]
+    } else if( cRE = stRE.slice( head.length).match(
+      /^(<!--.*?-->|[^<>]*)+(<[a-z][-\w]*[^<>]*>|<\/([a-z][-\w]*)>)/)) {
+      if (cRE[3]) {
+       if (tag != cRE[3]) return [false, stack+','+tag];
+       elems.push( indtE + (cRE[1]? '\n  '+ind+cRE[1] :'') +'\n  '+ind+'</'+cRE[3]+'>');
+       i += head.length + cRE[0].length
+      } else {
+       stack.push( tag);
+       elems.push( indtE + (cRE[1]? '\n  '+ind+cRE[1] :''));
+       i += head.length + cRE[1].length;
+       ind += ' '
+      }
+      !j && tag=='div'? j=elems.length: null
+     }
+    else return [false, stack+','+tag]
    } else {
-    if( t[2] && (!stack.length || '/'+(i=stack.pop()) != t[2]))
-     return [false, stack+','+t[2]];
-    ind = ind.slice(0,-1);
-    elems.push( ind + eHead);
-    i = nextI;
+    if( !stack.length || stack.pop() != tag) return [false, stack];
+    i += head.length;
+    elems.push( (ind= ind.slice(1)) + eHead);
    }
-  } else {
-   nextI = s.indexOf( '<',i);
-   let txL= (tx = s.substring( i,nextI)).length;
-   if (!txL || tx.indexOf( '>',i) >=0) return [false, stack]; 
-   i = nextI;
-   elems.push( txL >799 ? tx.substring( 0,210)+'.....'+tx.substring( txL-190) :tx)
+  } else if( cRE = stRE.match(/^(?:[^<>]+|<!--[^<>]*-->)+/s)) {
+   let l;
+   elems.push( ind +( (l=cRE[0].length) <730? cRE[0]: cRE[0].slice(0,190)+'.....'+cRE[0].slice(-170)))
+   i += l
   }
+  else return [false, 'inner '+stack];
  }
  return [!stack.length, 'the end; missing closing tag']
 }
@@ -75,12 +66,12 @@ slctr = s =>{
  s = s.replace(/<\/\w+>$/,'');
  const stack=[], selAtt=[];
  let chiLv=0, nthCh = new Array(33).fill(0),
- tx, nextI, i=0;
+ tx, nexti, i=0;
  for (;i < s.length;) {
   c = s[i];
   if (c == '<') {
-   nextI = s.indexOf( '>', ++i);
-   let eHead = s.substring( i, nextI++);
+   nexti = s.indexOf( '>', ++i);
+   let eHead = s.substring( i, nexti++);
    if (!(/^(?:meta|link|input|img|hr|base)\b|^!--.*--$/.test(eHead))) {
     let str,
     t = eHead.match(/^(\/?[a-z][-\w]*)(?:\s+(.+))?/),
@@ -88,7 +79,7 @@ slctr = s =>{
     atrbs = t[2];
     if (tag[0] != '/') {
      if( /script|style|title|path/.test(tag) ) {
-      i = s.indexOf( '</'+tag+'>', nextI);
+      i = s.indexOf( '</'+tag+'>', nexti);
       if (i>=0) { i += 3+tag.length; continue }
      }
      ++nthCh[ chiLv++];
@@ -112,8 +103,8 @@ slctr = s =>{
     }
    }
   }
-  else tx = s.substring( i, nextI = s.indexOf( '<', i));
-  i = nextI;
+  else tx = s.slice( i, nexti = s.indexOf( '<', i));
+  i = nexti;
   if (i<0) break
  }
  let p='';
@@ -123,11 +114,13 @@ slctr = s =>{
 }
 
 let cli = process.argv.slice(2).join(' ');
-if (!cli) cli= //<= our URL default
+if (!cli) {
+ cli = ''; // URL default here
+}
+fs = require('fs');
 
 (async ()=>{
  const
- fs = require('fs'),
  html=
  await (async ()=>{
   if (/^https?:\/\//.test(cli)) {
@@ -141,16 +134,20 @@ if (!cli) cli= //<= our URL default
    return await page.content()
   } else {
    try {
-    return fs.readFileSync( cli, 'utf8')
+    d = fs.readFileSync( cli, 'utf8');
+    console.log("Found file '"+cli+"'")
+    return d
    } catch (e){
-    console.error( 'Error: '+cli+': ' +(e.code =='ENOENT'? 'File not found': 'Error reading it')) }
+    console.error('Error: '+cli+': ' +
+     (e.code =='ENOENT'? 'File not found': 'Error reading it'))
+   }
   }})();
 
-stdout.cursorTo(0);stdout.clearLine();
+ stdout.cursorTo(0);stdout.clearLine();console.log('Validating its content as HTML...')
 
- let [ok,e] = await isElmBlncd( html);
+ let [ok,e] = await isHTMvalid( html);
  if (!ok) {
-  console.log('HTML document has inbalanced element at',e.replace(/,/g,' > ')); exit(1)}
+  console.log('Broken HTML document, suspected at',e.replace(/,/g,' > ')); exit(1)}
 
  let {Select} = await require('enquirer'), elmns = [...elems];
 lo:
@@ -178,8 +175,7 @@ lo:
    stdin.resume();
    stdin.once('data', d =>{ r(d[0]) })
   })) {
-   case 115:
-    fs.writeFileSync('a.html', html); break; // s hit //todo
+   case 115: fs.writeFileSync('a.html', html); break; // s hit //todo
    case 13:
    case 121: break; // Enter or y hit
    default: break lo;
